e0ead186ccccbf1c0fb9fc30c023b65d23ad879b
Steven Lacher <lacherclp@yahoo.com>
2015-02-22 15:51:49 -0500

    changed quotes to doubles

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,16 +5,13 @@ import com.bellifull.UserRole
 class BootStrap {
 
     def init = { servletContext ->
-		//def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
-		def adminRole = new Role(authority: 'ROLE_ADMIN').save()
-		def userRole = new Role(authority: 'ROLE_USER').save()
-		//def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+		def adminRole = new Role(authority: "ROLE_ADMIN").save(flush: true)
+		def userRole = new Role(authority: "ROLE_USER").save(flush: true)
   
-		def testUser = new User(username: 'me', password: 'password')
+		def testUser = new User(username: "me", password: "password")
 		testUser.save(flush: true)
   
 		UserRole.create testUser, adminRole, true
-	//	userRole.save(flush:true)
 		
 		assert User.count() == 1
 		assert Role.count() == 2
