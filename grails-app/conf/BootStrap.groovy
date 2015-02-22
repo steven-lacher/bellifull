e861781eefd8cd9f575b817b0e217a6d7aedf076
Steven Lacher <lacherclp@yahoo.com>
2015-02-22 15:38:57 -0500

    changes to role creation

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,8 +5,10 @@ import com.bellifull.UserRole
 class BootStrap {
 
     def init = { servletContext ->
-		def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
-		def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+		//def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+		def adminRole = new Role(authority: 'ROLE_ADMIN').save()
+		def userRole = new Role(authority: 'ROLE_USER').save()
+		//def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
   
 		def testUser = new User(username: 'me', password: 'password')
 		testUser.save(flush: true)
